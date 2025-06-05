ff16d686780bf44d524623e205e3560bbd3b5e60
ethelegend <michel.elijah.k@gmail.com>
2025-06-05 15:20:39 +1200

    1.1 Adding more functionality for people, including storage. Removed display features pending a decision on how best to implement.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,61 +5,87 @@ public class Main {
     static final Window window = new Window(); // The window where statistics and customisation happens
     private static final Simulation simulation = new Simulation(); // The window where the simulation is rendered
     private static Graphics graphics; // The graphics of the simulation
-    private static final int[][] stat = new int[3][2]; // The current stats. Rows of NORMAL, INFECTED, IMMUNE, columns of CHANGE, TOTAL
 
     // Simulation parameters
     private static int WIDTH = 100;
     private static int HEIGHT = 100;
     private static int PEOPLE = 30;
     private static int INFECTED = 1;
+    private static double INFECTION_CHANCE = 0.5;
     private static int INFECTION_COOLDOWN = 10;
     private static int IMMUNITY_COOLDOWN = 10;
+
+    private static Person[] row;
+    private static Person[] column;
 
     public static void main(String[] args) {
         window.setVisible(true);
         simulation.setVisible(true);
         graphics = simulation.getGraphics();
 
+        row = new Person[HEIGHT];
+        column = new Person[WIDTH];
+
         for (int i = 0; i < PEOPLE; i++) { // Generates the people
-            new Person(WIDTH,HEIGHT,(i < INFECTED));
+            if (Math.random() < 0.5) {
+                new Person(true, HEIGHT, WIDTH, (i < INFECTED) ? 1 : 0);
+            } else {
+                new Person(false, WIDTH, HEIGHT, (i < INFECTED) ? 1 : 0);
+            }
         }
-        window.update();
     }
 
     private static class Person { // Stores a person in the simulation
         // Position
-        private int x;
-        private int y;
-
-        private int state = 0; // Current state. Corresponds to stat[][], defaults to NORMAL
-        private int cooldown = 0; // How many ticks until the state is updated
-
-        private Person(int x, int y, boolean infected) {
-            this.x = x;
-            this.y = y;
-            stat[0][0] ++;
-            if (infected) {
-                changeState();
+        private final boolean horizontal;
+        private final int lane;
+        private int pos;
+        private boolean forwards;
+        private int state; // Current state. == 0 -> normal, <= INFECTION_COOLDOWN -> infected, else -> immune, wraps back to 0
+        private boolean infected = false;
+        private final Person pointer;
+        private Person(boolean horizontal, int lane, int pos, int state) {
+            this.horizontal = horizontal;
+            this.lane = lane;
+            this.pos = pos;
+            this.pointer = (horizontal ? row : column)[lane];
+            (horizontal ? row : column)[lane] = this;
+            forwards = (Math.random() < 0.5);
+            this.state = state;
+        }
+        private void move() {
+            if (pos == 0) {
+                forwards = true;
+            } else if (pos == (horizontal ? WIDTH : HEIGHT) - 1) {
+                forwards = false;
+            }
+            if (forwards) {
+                pos ++;
+            } else {
+                pos --;
             }
         }
-        private void update() {
+        private void preInfection() {
             if (state != 0) {
-                cooldown --;
-                if (cooldown == 0) {
-                    changeState();
-                }
+                if (state <= INFECTION_COOLDOWN) infect();
+                state ++;
             }
         }
-        // Called whenever the state needs to be updated
-        private void changeState() {
-            stat[state][0] --; // The current state lost one member
-            state = (state + 1) % 3; // Shifts the state
-            stat[state][0] ++; // The new state gained one member
-            if (state == 1) { // Sets the cooldown for infected
-                cooldown = INFECTION_COOLDOWN;
-            } else if (state == 2) { // Sets the cooldown for immune
-                cooldown = IMMUNITY_COOLDOWN;
+        private void infect() {
+            Person person = (horizontal ? row : column)[lane];
+            while (person != null) {
+                if (person.state == 0 && pos == person.pos && Math.random() < INFECTION_CHANCE) person.infected = true;
+                person = person.pointer;
             }
+            person = (horizontal ? column : row)[pos];
+            while (person != null) {
+                if (person.state == 0 && lane == person.pos && Math.random() < INFECTION_CHANCE) person.infected = true;
+                person = person.pointer;
+            }
+        }
+        private void postInfection() {
+            if (infected) state = 1;
+            else if (state > INFECTION_COOLDOWN + IMMUNITY_COOLDOWN) state = 0;
         }
     }
 
@@ -98,15 +124,6 @@ public class Main {
             JLabel l = new JLabel(text);
             l.setHorizontalAlignment(SwingConstants.CENTER);
             add(l);
-        }
-
-        private void update() { // Updates stat[][] and statLabel[][]
-            for (int i = 0; i < statLabel.length; i++) {
-                statLabel[i][0].setText(Integer.toString(stat[i][0]));
-                stat[i][1] += stat[i][0];
-                statLabel[i][1].setText(Integer.toString(stat[i][1]));
-                stat[i][0] = 0;
-            }
         }
     }
 
