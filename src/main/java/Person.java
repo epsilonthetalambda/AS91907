67d1a4e0fb15c46d93ae68cc57e43f818ef9ce72
ethelegend <michel.elijah.k@gmail.com>
2025-06-14 17:34:56 +1200

    1.3.1 Fixes an issue where people in the Normal state get infected regardless of if they should be

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -61,8 +61,10 @@ public class Person {
         if (infected) {
             infected = false;
             state = 1;
-        } else if (state >= Main.IMMUNITY_COOLDOWN) state = 0;
-        else state ++;
+        } else if (state > 0) {
+            state ++;
+            if (state > Main.IMMUNITY_COOLDOWN) state = 0;
+        }
         return state();
     }
 }
