92f529de7580a5082cdb5b2fd60a1f1e5451c305
ethelegend <michel.elijah.k@gmail.com>
2025-08-02 15:05:12 +1200

    5.6
    Refactored the main simulation to be separated into separate panes. This allowed the simulation colours to be global instead of per-simulation, and made the launcher slightly less cluttered. Also added comments, as this is likely the final update the program will receive.

--- src/main/java/Image.java
+++ src/main/java/Image.java
@@ -1,13 +1,18 @@
+/*
+ *  AS91907.Image
+ *  Last Updated: 10/07/2025
+ *  Purpose: An Image that stores a custom drawing function, for use in Renders.
+ */
+
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 public abstract class Image extends BufferedImage { // Used to create custom graphics in Render
-    public final Simulation s;
-    public final int w, h;
-    public final Graphics g; // Stores its graphics
-    public Image(Simulation s, int w, int h) {
-        super(w, h, TYPE_INT_ARGB);
-        this.s = s; this.w = w; this.h = h;
+    public final int w, h; // Width and height of the image
+    public final Graphics g; // Stores its graphics as a final reference
+    public Image(int w, int h) {
+        super(w, h, TYPE_INT_ARGB); // Initialises the buffered image
+        this.w = w; this.h = h;
         g = getGraphics();
         render();
     }
